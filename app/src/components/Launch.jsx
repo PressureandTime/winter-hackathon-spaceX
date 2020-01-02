bbe38cee89f99b2746a5f05bb2dddbb016633e17
FreedomWriter <natalienetworks@gmail.com>
2020-01-02 17:25:09 -0600

    rendering launch accordian

--- app/src/components/Launch.jsx
+++ app/src/components/Launch.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%"
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular
+  }
+}));
 
 function Launch({ launch }) {
-  return <h1>Hello</h1>;
+  const classes = useStyles();
+
+  const {
+    mission_name,
+    launch_year,
+    launch_date_utc,
+    launch_success,
+    details
+  } = launch;
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>{mission_name}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <div>
+            <div>
+              <p>{launch_year}</p>
+              <p>Mission: {launch_success ? `Success` : `Failure`}</p>
+              <p>{details}</p>
+            </div>
+            <Button>Details</Button>
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
 }
 
 export default Launch;
