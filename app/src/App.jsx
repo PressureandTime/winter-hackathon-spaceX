ca0d2234f41620df8fbd8046c57f8c9be6e343eb
FreedomWriter <natalienetworks@gmail.com>
2020-01-02 13:50:43 -0600

    added redux-logger

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <HomePage />
+      </Switch>
     </div>
   );
 };
