b597120ed039f62511fbd98141b64348bc762130
cecil john tantay <cjbtantay@gmail.com>
2020-01-02 11:36:29 -0800

    brought everything to basics, removed past code

--- app/src/index.js
+++ app/src/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import BalanceReducer from './store/BalanceReducer';
-import LoanReducer from './store/LoanReducer';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './store';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-
-const store = createStore(
-	combineReducers({
-        BalanceReducer,
-        LoanReducer
-
-	}),
-	applyMiddleware(thunk),
-);
-
-
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-function HomePage () {
-	const balance = useSelector((state) => state.BalanceReducer.balance);
-	const loan = useSelector((state) => state.LoanReducer.loan);
-	const dispatch = useDispatch();
-
-	function applyLoanHandle () {
-		dispatch({ type: 'APPLY' });
-	}
-	return (
-		<div>
-			<h2>Balance: {balance}</h2>
-			<h2>{loan ? 'Loan Applied' : 'Loan Needed'}</h2>
-			<button className="button-style" disabled={loan ? true : false} onClick={applyLoanHandle}>
-			{loan? "Loan Applied" : "Apply for loan"}
-			</button>
-		</div>
-	);
+function HomePage() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  );
 }
 
 export default HomePage;
