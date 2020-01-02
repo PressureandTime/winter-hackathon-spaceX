3d577ae768b104849d0419c22690456eb23d421c
FreedomWriter <natalienetworks@gmail.com>
2020-01-02 13:45:45 -0600

    exported App

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import HomePage from './pages/HomePage';
+import React from "react";
+import HomePage from "./pages/HomePage";
 
-export const App = () => {
+const App = () => {
   return (
     <div>
       <HomePage />
     </div>
   );
 };
+
+export default App;
