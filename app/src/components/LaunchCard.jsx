0458982b131a7a003298f5f81b2e4ffd5ed67b08
Henry Neal <henry.neal0@gmail.com>
2020-01-03 12:06:08 -0500

    added stylings for social, recovered lost code, built out prev and next for missions

--- app/src/components/LaunchCard.jsx
+++ app/src/components/LaunchCard.jsx
@@ -1,95 +1,95 @@
-import React, { useEffect } from 'react';
-import moment from 'moment';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+// import moment from 'moment';
+import { useDispatch, useSelector } from "react-redux";
 
-import Carousel from 'react-material-ui-carousel';
+import Carousel from "react-material-ui-carousel";
 
-import { fetchOneLaunch } from '../store/action';
+import { fetchOneLaunch } from "../store/action";
 
-import spacexPlaceholder from '../img/spacexPlaceholder.png';
+import spacexPlaceholder from "../img/spacexPlaceholder.png";
 
 const LaunchCard = ({ match }) => {
-  const dispatch = useDispatch();
-  const launch = useSelector(state => state.launchesReducer.launch);
+	const dispatch = useDispatch();
+	const launch = useSelector(state => state.launchesReducer.launch);
 
-  const {
-    details,
-    launch_date_utc,
-    launch_failure_details,
-    launch_site,
-    launch_success,
-    links,
-    mission_name,
-    rocket,
-    ships
-  } = launch;
+	const {
+		details,
+		launch_date_utc,
+		launch_failure_details,
+		launch_site,
+		launch_success,
+		links,
+		mission_name,
+		rocket,
+		ships
+	} = launch;
 
-  console.log(launch);
+	console.log(launch);
 
-  useEffect(() => {
-    dispatch(fetchOneLaunch(match.params.id));
-  }, [match.params.id, dispatch]);
+	useEffect(() => {
+		dispatch(fetchOneLaunch(match.params.id));
+	}, [match.params.id, dispatch]);
 
-  if (Object.entries(launch).length === 0) {
-    return <p>LOADING</p>;
-  }
+	if (Object.entries(launch).length === 0) {
+		return <p>LOADING</p>;
+	}
 
-  return (
-    <>
-      {links.flickr_images.length === 0 && !links.mission_patch_small && (
-        <img src={spacexPlaceholder} alt={'Rocket Launch with SpaceX Logo'} />
-      )}
+	return (
+		<>
+			{links.flickr_images.length === 0 && !links.mission_patch_small && (
+				<img src={spacexPlaceholder} alt={"Rocket Launch with SpaceX Logo"} />
+			)}
 
-      {links.flickr_images.length === 0 && links.mission_patch_small && (
-        <img
-          src={links.mission_patch_small}
-          alt={'Patch for ' + mission_name}
-        />
-      )}
+			{links.flickr_images.length === 0 && links.mission_patch_small && (
+				<img
+					src={links.mission_patch_small}
+					alt={"Patch for " + mission_name}
+				/>
+			)}
 
-      {launch.links.flickr_images.length > 0 && (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Carousel>
-            {launch.links.flickr_images.map((img, index) => (
-              <img
-                key={index}
-                src={img}
-                alt={'Launch'}
-                style={{ height: 'auto', width: '500px' }}
-              />
-            ))}
-          </Carousel>
-        </div>
-      )}
+			{launch.links.flickr_images.length > 0 && (
+				<div style={{ display: "flex", justifyContent: "center" }}>
+					<Carousel>
+						{launch.links.flickr_images.map((img, index) => (
+							<img
+								key={index}
+								src={img}
+								alt={"Launch"}
+								style={{ height: "auto", width: "500px" }}
+							/>
+						))}
+					</Carousel>
+				</div>
+			)}
 
-      <h1>{mission_name}</h1>
+			<h1>{mission_name}</h1>
 
-      <h2>{moment(launch_date_utc).format('MMMM Do YYYY, h:mm:ss a')}</h2>
+			{/* <h2>{moment(launch_date_utc).format('MMMM Do YYYY, h:mm:ss a')}</h2> */}
 
-      <h3>SITE: {launch_site.site_name}</h3>
+			<h3>SITE: {launch_site.site_name}</h3>
 
-      <h3>
-        The launch was a{' '}
-        {launch_success ? `success!` : `miserable, disgusting failure!`}
-      </h3>
+			<h3>
+				The launch was a{" "}
+				{launch_success ? `success!` : `miserable, disgusting failure!`}
+			</h3>
 
-      {rocket && (
-        <>
-          <p>Rocket Name: {rocket.rocket_name}</p>{' '}
-          <p>Rocket Type: {rocket.rocket_type} </p>
-        </>
-      )}
+			{rocket && (
+				<>
+					<p>Rocket Name: {rocket.rocket_name}</p>{" "}
+					<p>Rocket Type: {rocket.rocket_type} </p>
+				</>
+			)}
 
-      {ships.length > 0 && <h3>SHIPS: </h3>}
-      {ships && ships.map((item, index) => <p key={index}>{item}</p>)}
+			{ships.length > 0 && <h3>SHIPS: </h3>}
+			{ships && ships.map((item, index) => <p key={index}>{item}</p>)}
 
-      {details && <p>Fun Fact: {details}.</p>}
+			{details && <p>Fun Fact: {details}.</p>}
 
-      {launch_failure_details && launch_failure_details.reason && (
-        <p>The launch failed due to a {launch_failure_details.reason}.</p>
-      )}
-    </>
-  );
+			{launch_failure_details && launch_failure_details.reason && (
+				<p>The launch failed due to a {launch_failure_details.reason}.</p>
+			)}
+		</>
+	);
 };
 
 export default LaunchCard;
--- app/src/components/Missions/MissionList.jsx
+++ app/src/components/Missions/MissionList.jsx
@@ -1,44 +1,60 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-import { fetchMissionsList } from '../../store/action/index';
+import {
+	fetchMissionsList,
+	fetchPrevTenMissions,
+	fetchNextTenMissions
+} from "../../store/action/index";
 
-import Mission from './Mission';
-import Search from '../Search';
+import Mission from "./Mission";
+import Search from "../Search";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%'
-  }
+	root: {
+		width: "100%"
+	},
+	buttonContainer: {
+		display: "flex",
+		justifyContent: "center"
+	}
 }));
 function MissionList() {
-  const dispatch = useDispatch();
-  const missions = useSelector(state => {
-    return state.missionsReducer.missions;
-  });
-  const searchedMissions = useSelector(state => {
-    return state.missionsReducer.searchedMissions;
-  });
-  const classes = useStyles();
+	const dispatch = useDispatch();
+	const missions = useSelector(state => {
+		return state.missionsReducer.missions;
+	});
+	const searchedMissions = useSelector(state => {
+		return state.missionsReducer.searchedMissions;
+	});
+	const classes = useStyles();
 
-  useEffect(() => {
-    dispatch(fetchMissionsList());
-  }, []);
-  return (
-    <>
-      <Search searchType="missions" />
-      <div style={{ marginTop: '50px' }} className={classes.root}>
-        {searchedMissions && searchedMissions.length > 0
-          ? searchedMissions.map(mission => (
-              <Mission key={mission.mission_id} mission={mission} />
-            ))
-          : missions.map(mission => (
-              <Mission key={mission.mission_id} mission={mission} />
-            ))}
-      </div>
-    </>
-  );
+	useEffect(() => {
+		dispatch(fetchMissionsList());
+	}, []);
+	return (
+		<>
+			<Search searchType="missions" />
+
+			<div style={{ marginTop: "50px" }} className={classes.root}>
+				<div className={classes.buttonContainer}>
+					<Button onClick={() => dispatch(fetchPrevTenMissions())}>
+						Previous
+					</Button>
+					<Button onClick={() => dispatch(fetchNextTenMissions())}>Next</Button>
+				</div>
+				{searchedMissions && searchedMissions.length > 0
+					? searchedMissions.map(mission => (
+							<Mission key={mission.mission_id} mission={mission} />
+					  ))
+					: missions.map(mission => (
+							<Mission key={mission.mission_id} mission={mission} />
+					  ))}
+			</div>
+		</>
+	);
 }
 
 export default MissionList;
