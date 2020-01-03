767d2ee7a6e6e993b5459bf084cfb8f52b862e9f
FreedomWriter <natalienetworks@gmail.com>
2020-01-03 03:03:38 -0600

    working on carousel bugs

--- app/src/components/History.jsx
+++ app/src/components/History.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchHistory, fetchOneLaunch } from "../store/action";
+import { Link } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -8,14 +9,20 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 const useStyles = makeStyles({
   card: {
-    width: "40%",
-    height: 300,
-    margin: "5% auto",
+    width: "45%",
+    height: 275,
+    margin: "10% 5%",
     fontSize: 24,
-    overflow: "scroll"
+    overflow: "hidden",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+    // padding: "2%"
   },
   bullet: {
     display: "inline-block",
@@ -23,7 +30,7 @@ const useStyles = makeStyles({
     transform: "scale(0.8)"
   },
   title: {
-    fontSize: 34
+    fontSize: 28
   },
   pos: {
     marginBottom: 12
@@ -32,22 +39,37 @@ const useStyles = makeStyles({
     display: "flex",
     flexDirection: "row nowrap"
   },
-  buttonContainer: {
+  linkContainer: {
     display: "flex",
     flexDirection: "row nowrap",
     justifyContent: "space-around",
     marginTop: "10%"
   },
+  aTags: {
+    textDecoration: "none",
+    fontSize: 12,
+    cursor: "pointer",
+    color: "white"
+  },
   imgSizing: {
     width: "100%",
-    height: "100%"
+    padding: 0
+  },
+  iconBack: {
+    marginTop: "230px",
+    marginRight: "-5%"
+  },
+  iconForward: {
+    marginTop: "230px",
+    marginLeft: "-4.5%",
+    marginRight: "5%"
   }
 });
 
 function History() {
   const [historyState, setHistoryState] = useState(1);
-
   const [imageState, setImageState] = useState(1);
+
   const dispatch = useDispatch();
   const history = useSelector(state => state.historyReducer.history);
 
@@ -69,23 +91,51 @@ function History() {
   picsArr && console.log(`Launches: `, picsArr.flickr_images);
 
   useEffect(() => {
+    //rework so doesn't perform an axios call every 10 seconds
     dispatch(fetchHistory(historyState));
     dispatch(fetchOneLaunch(65));
     setTimeout(() => {
-      historyState <= 18
-        ? setHistoryState(historyState + 1)
+      const prevHistoryState = historyState;
+      const prevImageState = imageState;
+      prevHistoryState <= 15
+        ? setHistoryState(prevHistoryState + 1)
         : setHistoryState(1);
-      imageState <= 4 ? setImageState(imageState + 1) : setImageState(0);
-    }, 5000);
+      prevImageState <= 4
+        ? setImageState(prevImageState + 1)
+        : setImageState(0);
+    }, 10000);
   }, [historyState, imageState]);
+
+  const handleBack = () => {
+    const prevState = historyState;
+    historyState !== 1 ? setHistoryState(prevState - 1) : setHistoryState(18);
+  };
+
+  const handleForward = () => {
+    const prevState = historyState;
+    historyState !== 18 ? setHistoryState(prevState + 1) : setHistoryState(1);
+  };
 
   console.log(`History: `, history);
   return (
     <div className={classes.layout}>
       <Card className={classes.card} variant="outlined">
+        {picsArr && (
+          <div>
+            <img
+              className={classes.imgSizing}
+              src={picsArr.flickr_images[imageState]}
+              alt="flickr"
+              onClick={() => console.log("IMAGE CLICKED!!!")}
+            />
+          </div>
+        )}
+      </Card>
+      <ArrowBackIosIcon onClick={handleBack} className={classes.iconBack} />
+      <Card className={classes.card} variant="outlined">
         <CardContent>
           <Typography className={classes.pos} color="textSecondary">
-            On{" "}
+            On
             {event_date_utc &&
               event_date_utc.replace("T", " at ").replace("Z", "")}
           </Typography>
@@ -98,33 +148,26 @@ function History() {
           >
             {title}
           </Typography>
-          <Typography className={classes.pos} color="textSecondary">
+          {/* <Typography className={classes.pos} color="textSecondary">
             Flight Number: {flight_number}
-          </Typography>
+          </Typography> */}
           <Typography variant="body2" component="p">
             {details}
           </Typography>
-        </CardContent>
-        {/* <CardActions className={classes.buttonContainer}>
-          <Button size="small" onClick={() => }>
-            Article
-          </Button>
-          <Button size="small" onClick={links.wikipedia}>
-            Wikipedia
-          </Button>
-        </CardActions> */}
-      </Card>
-      <Card className={classes.card} variant="outlined">
-        {picsArr && (
-          <div>
-            <img
-              className={classes.imgSizing}
-              src={picsArr.flickr_images[imageState]}
-              alt="flickr"
-            />
+          <div className={classes.linkContainer}>
+            <a href={links.article} target="_blank" className={classes.aTags}>
+              Read More
+            </a>
+            <a href={links.wikipedia} target="_blank" className={classes.aTags}>
+              Wiki
+            </a>
           </div>
-        )}
+        </CardContent>
       </Card>
+      <ArrowForwardIosIcon
+        onClick={handleForward}
+        className={classes.iconForward}
+      />
     </div>
   );
 }
