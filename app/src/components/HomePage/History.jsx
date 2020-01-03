5880fa439975e522fcab492a01043597481814aa
FreedomWriter <natalienetworks@gmail.com>
2020-01-03 11:53:54 -0600

    added loading spinner to history components

--- app/src/components/HomePage/History.jsx
+++ app/src/components/HomePage/History.jsx
@@ -87,7 +87,9 @@ function History() {
     return state.launchesReducer.launch.links;
   });
 
-  picsArr && console.log(`Launches: `, picsArr.flickr_images);
+  const { launchesLoading } = useSelector(state => ({
+    launchesLoading: state.launchesReducer.loading
+  }));
 
   useEffect(() => {
     dispatch(fetchHistory());
@@ -96,71 +98,88 @@ function History() {
 
   return (
     <div className={classes.layout}>
-      {picsArr && picsArr.flickr_images.length > 0 && (
-        <div className={classes.cardContainer}>
-          <Carousel>
-            {picsArr.flickr_images.map((img, index) => (
-              <img
-                key={index}
-                src={img}
-                alt={"Launch"}
-                className={classes.card}
-              />
-            ))}
-          </Carousel>
-        </div>
+      {launchesLoading ? (
+        <Loading />
+      ) : (
+        picsArr &&
+        picsArr.flickr_images.length > 0 && (
+          <div className={classes.cardContainer}>
+            <Carousel>
+              {picsArr.flickr_images.map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+                  alt={"Launch"}
+                  className={classes.card}
+                />
+              ))}
+            </Carousel>
+          </div>
+        )
       )}
-      {picsArr && picsArr.flickr_images.length > 0 && (
-        <div className={classes.cardContainer}>
-          <Carousel>
-            {history &&
-              history.map((el, index) => {
-                return (
-                  <Card className={classes.card} variant="outlined" key={index}>
-                    <CardContent>
-                      <Typography className={classes.pos} color="textSecondary">
-                        On
-                        {el.event_date_utc &&
-                          el.event_date_utc
-                            .replace("T", " at ")
-                            .replace("Z", "")}
-                      </Typography>
-                      <Typography
-                        className={classes.title}
-                        color="textSecondary"
-                        gutterBottom
-                        variant="h5"
-                        component="h2"
-                      >
-                        {el.title}
-                      </Typography>
-                      <div className={classes.details}>
-                        <Typography variant="body2" component="p">
-                          {el.details}
+      {launchesLoading ? (
+        <Loading />
+      ) : (
+        picsArr &&
+        picsArr.flickr_images.length > 0 && (
+          <div className={classes.cardContainer}>
+            <Carousel>
+              {history &&
+                history.map((el, index) => {
+                  return (
+                    <Card
+                      className={classes.card}
+                      variant="outlined"
+                      key={index}
+                    >
+                      <CardContent>
+                        <Typography
+                          className={classes.pos}
+                          color="textSecondary"
+                        >
+                          On
+                          {el.event_date_utc &&
+                            el.event_date_utc
+                              .replace("T", " at ")
+                              .replace("Z", "")}
                         </Typography>
-                      </div>
-                      <div className={classes.linkContainer}>
-                        <a
-                          href={el.links.article}
-                          target="_blank"
-                          className={classes.aTags}
+                        <Typography
+                          className={classes.title}
+                          color="textSecondary"
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
                         >
-                          Read More
-                        </a>
-                        <a
-                          href={el.links.wikipedia}
-                          target="_blank"
-                          className={classes.aTags}
-                        >
-                          Wiki
-                        </a>
-                      </div>
-                    </CardContent>
-                  </Card>
-                );
-              })}
-          </Carousel>
-        </div>
+                          {el.title}
+                        </Typography>
+                        <div className={classes.details}>
+                          <Typography variant="body2" component="p">
+                            {el.details}
+                          </Typography>
+                        </div>
+                        <div className={classes.linkContainer}>
+                          <a
+                            href={el.links.article}
+                            target="_blank"
+                            className={classes.aTags}
+                          >
+                            Read More
+                          </a>
+                          <a
+                            href={el.links.wikipedia}
+                            target="_blank"
+                            className={classes.aTags}
+                          >
+                            Wiki
+                          </a>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  );
+                })}
+            </Carousel>
+          </div>
+        )
       )}
     </div>
   );
